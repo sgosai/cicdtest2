adbe81e72db75ba6a1ad9e7b34fe3e2c08639787
Shailan Gosai <sgosai@m-r27lnxr2pr.paloaltonetworks.local>
2026-08-26 12:25:29 -0600

    create unit test

---
+++ Packs/multiply/Scripts/multiply_test.py
@@ -0,0 +1,26 @@
+import pytest
+from multiply import multiply_logic
+# from CommonServerPython import CommandResults
+
+@pytest.mark.parametrize(
+    "args, expected_value", 
+    [
+        ({'num1': '5', 'num2': '3'}, 15),
+        ({'num1': '10', 'num2': '2'}, 10),
+        ({'num1': '-2', 'num2': '4'}, -80)
+    ]
+)
+def test_multiply_logic(args, expected_value):
+    """
+    Given: Arguments provided to the XSOAR command.
+    When: Running the multiply_logic function.
+    Then: Ensure the correct integer multiplication is returned in the CommandResults.
+    """
+    # Run the function
+    result = multiply_logic(args)
+    
+    # Assert the outputs match expected behavior
+    # assert result.outputs['Value'] == expected_value
+    # assert f'The result is {expected_value}' in result.readable_output
+    assert result == expected_value
+    # assert f'The result is {expected_value}' in result.readable_output
